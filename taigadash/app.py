c7deebd380d941ef3c3d9b98edbbfc03193aefa7
IvanBrasilico <brasilico.ivan@gmail.com>
2020-08-31 17:23:14 -0300

    1000 linhas

--- taigadash/app.py
+++ taigadash/app.py
@@ -1,12 +1,12 @@
-import psycopg2
 import pandas as pd
+import psycopg2
 from flask import Flask
 
 SQL_ISSUES = '''
 select p.id, s.name, i.subject, i.created_date, i.modified_date from issues_issue i 
 inner join projects_project p on p.id = i.project_id
 inner join projects_taskstatus s on s.id = i.status_id
-limit 10;
+limit 1000;
 '''
 
 con = psycopg2.connect(database='taiga', user='taiga_consulta')
@@ -14,8 +14,8 @@ df = pd.read_sql(SQL_ISSUES, con=con)
 # con.close()
 print(df.head())
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/taigadash/')
