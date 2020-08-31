c2a40f14adf584c659aab4b95a464380a6261f7b
IvanBrasilico <brasilico.ivan@gmail.com>
2020-08-31 15:39:41 -0300

    Setup e primeiro teste PostgreSQL

--- taigadash/app.py
+++ taigadash/app.py
@@ -1,3 +1,5 @@
+import psycopg2
+
 SQL_ISSUES = '''
 select p.id, s.name, i.subject, i.created_date, i.modified_date from issues_issue i 
 inner join projects_project p on p.id = i.project_id
@@ -5,4 +7,10 @@ inner join projects_taskstatus s on s.id = i.status_id
 limit 10;
 '''
 
-
+con = psycopg2.connect(host='localhost', database='taiga', user='taiga_consulta')
+cur = con.cursor()
+cur.execute(SQL_ISSUES)
+recset = cur.fetchall()
+for rec in recset:
+    print (rec)
+con.close()
