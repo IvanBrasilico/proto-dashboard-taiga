956a8009e4ef48234dc3089a6dc2c315533aa617
IvanBrasilico <brasilico.ivan@gmail.com>
2020-09-01 08:26:54 -0300

    Testes rodando e relatório code coverage

--- taigadash/app.py
+++ taigadash/app.py
@@ -3,7 +3,7 @@ import sqlite3
 
 import pandas as pd
 import psycopg2
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
 from taigadash.db import SQL_ISSUES
 from taigadash.forms.filtro_form import FiltroForm
@@ -18,15 +18,6 @@ print(df.head())
 print(df.to_dict())
 
 
-def create_app(df):
-    app = Flask(__name__)
-    app.config['df'] = df
-    return app
-
-
-app = create_app(df)
-
-
 def filter_df(df, status):
     filtered_df = df.copy()
     if status:
@@ -34,35 +25,44 @@ def filter_df(df, status):
     return filtered_df
 
 
-@app.route('/')
-@app.route('/taigadash/')
-def home():
-    df = app.config['df']
-    status = request.args.get('status')
-    filtered_df = filter_df(df, status)
-    return filtered_df.to_html()
+def create_app(df):
+    app = Flask(__name__)
+    app.config['df'] = df
+
+    @app.route('/')
+    @app.route('/taigadash/')
+    def home():
+        df = app.config['df']
+        status = request.args.get('status')
+        filtered_df = filter_df(df, status)
+        return filtered_df.to_html()
+
+    @app.route('/')
+    @app.route('/taigadash/html')
+    def html():
+        df = app.config['df']
+        status = request.args.get('status')
+        oform = FiltroForm()
+        filtered_df = filter_df(df, status)
+
+        return render_template('home.html',
+                               oform=oform,
+                               dados=filtered_df.to_dict())
+
+    @app.route('/taigadash/json')
+    def json():
+        df = app.config['df']
+        if request.json:
+            status = request.json.get('status')
+        else:
+            status = None
+        filtered_df = filter_df(df, status)
+        return jsonify(filtered_df.to_json()), 200
+
+    return app
 
 
-@app.route('/')
-@app.route('/taigadash/html')
-def html():
-    df = app.config['df']
-    status = request.args.get('status')
-    oform = FiltroForm()
-    filtered_df = filter_df(df, status)
-
-    return render_template('home.html',
-                           oform=oform,
-                           dados=filtered_df.to_dict())
-
-
-@app.route('/taigadash/json')
-def json():
-    df = app.config['df']
-    status = request.json.get('status')
-    filtered_df = filter_df(df, status)
-    return filtered_df.to_json()
-
+app = create_app(df)
 
 if __name__ == '__main__':
-    app.run(port=5010)
+    app.run(port=5010, debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,11 @@
 import unittest
-import pandas as pd
-from flask import Flask
 
+import pandas as pd
 from sqlalchemy import create_engine
 
+from taigadash.app import create_app
 from taigadash.db import create_test_base, SQL_ISSUES
-from testes.teste_base import RESULT, RESULT_JSON
+from tests.test_base import RESULT_JSON
 
 
 class AppTestCase(unittest.TestCase):
@@ -14,8 +14,7 @@ class AppTestCase(unittest.TestCase):
         self.engine = create_engine('sqlite:///testes.db')
         create_test_base(self.engine)
         df = pd.read_sql(SQL_ISSUES, con=self.engine)
-        app = Flask(__name__)
-        app.config['df'] = df
+        app = create_app(df)
         self.app = app.test_client()
 
     def test_taigadash_json(self):
